82a15d3dd10ac6b152558cb2bd32315cd31f9f3d
Lenard Szolnoki <lenards@speechmatics.com>
2022-08-18 10:54:23 +0100

    Finish implementation of Pipeline API
    TODO:
    * better comments
    * better names

--- include/pipeline_orchestrator_decl.h
+++ include/pipeline_orchestrator_decl.h
@@ -1,11 +1,13 @@
 #pragma once
 #include <array>
 #include <algorithm>
+#include <atomic>
 #include <cstddef>
 #include <memory>
 #include <string>
 #include <functional>
 #include <string_view>
+#include <thread>
 #include <tuple>
 #include <type_traits>
 #include <utility>
@@ -80,14 +82,37 @@ template <typename ComponentName, typename out, typename ...in, typename ...Inpu
 pipeline_module(ComponentName, std::function<out(in...)>, InputName...)
     -> pipeline_module<ComponentName, std::tuple<InputName...>, out, in...>;
 
+template <typename T, std::size_t N>
+struct static_vector {
+  std::array<T, N> buf{};
+  std::size_t size = 0;
+
+  constexpr void push_back(T t) {
+    buf[size] = t;
+    ++size;
+  }
+};
+
+struct input_backref {
+  std::size_t pm_idx{};
+  std::size_t input_idx{};
+};
+
 template <typename... PM>
 struct Pipeline {
     std::tuple<typename PM::component_type...> components;
     // changed out_type to output_type
     using pipeline_buffers_t = std::tuple<std::conditional_t<std::is_same_v<void, typename PM::output_type>, std::monostate, std::shared_ptr<PipelineBuffer<typename PM::output_type>>>...>;
     pipeline_buffers_t pipeline_buffers;
+    std::atomic_bool sig{true};
+    std::array<std::thread, sizeof... (PM)> threads;
 
     Pipeline(PM... pm);
+    ~Pipeline() {
+      for (auto& thread: threads) {
+        thread.join();
+      }
+    }
 
     void start();
     
@@ -98,29 +123,57 @@ struct Pipeline {
             // changed pm.component_name
             return static_map{{kv{PM::component_name_type::value, Idx}...}};
         }(std::index_sequence_for<PM...>{});
-        return pipeline_buffers_t{
-            [&]{
-                if constexpr (std::is_same_v<void, typename PM::output_type>) {
-                    return std::monostate{};
-                } else {
-                    // Iterate over pms
-                    return std::apply(
-                        // Get component input names and find their indices
-                        [&](auto... input_names){
-                            // Use the index sequence to generate the input port for the particular input_name
-                            return [&]<std::size_t ...Idx>(std::index_sequence<Idx...>){
-                                return PipelineBuffer<typename PM::output_type>::PipelineBuffer_factory(
-                                    // Use indices from map to index into auto& components to get component references
-                                    // Build comp_refs for each component and use all of these to create the PipelineBuffer
-                                    component_output_ref{std::get<names_to_indices.get(PM::component_name_type::value)>(components)},
-                                    component_input_ref{component_input_ref_factory<Idx>(std::get<names_to_indices.get(input_names.value)>(components))}...
-                                );
-                            }(std::make_index_sequence<sizeof...(input_names)>{});
-                        },
-                        typename PM::input_name_tuple_type{}
-                    );
+
+        constexpr auto x = [&]<std::size_t... Idx>(
+            std::index_sequence<Idx...>) {
+            std::array<static_vector<input_backref, sizeof...(PM)>,
+                       sizeof...(PM)>
+                ret;
+            (std::apply(
+                 [&](auto... input_names) {
+                   return [&]<std::size_t... Idx2>(
+                       std::index_sequence<Idx2...>, std::size_t idx) {
+                     (
+                         [&] {
+                           constexpr auto input_idx =
+                               names_to_indices.get(input_names.value);
+                           ret[input_idx].push_back(input_backref{idx, Idx2});
+                         }(),
+                         ...);
+                   }
+                   (std::make_index_sequence<sizeof...(input_names)>{}, Idx);
+                 },
+                 typename PM::input_name_tuple_type{}),
+             ...);
+            return ret;
+        }
+        (std::index_sequence_for<PM...>{});
+
+        return [&]<std::size_t... PM_Idx>(std::index_sequence<PM_Idx...>) {
+            return pipeline_buffers_t{[&] {
+              if constexpr (std::is_same_v<void, typename PM::output_type>) {
+                return std::monostate{};
+              } else {
+                return [&]<std::size_t... Idx, std::size_t pm_idx>(
+                    std::index_sequence<Idx...>, std::index_sequence<pm_idx>) {
+                  return PipelineBuffer<typename PM::output_type>::
+                      PipelineBuffer_factory(
+                          component_output_ref{std::get<names_to_indices.get(
+                              PM::component_name_type::value)>(components)},
+                          [&] {
+                            constexpr std::size_t input_idx =
+                                x[pm_idx].buf[Idx].input_idx;
+                            constexpr std::size_t component_idx =
+                                x[pm_idx].buf[Idx].pm_idx;
+                            return component_input_ref_factory<input_idx>(
+                                std::get<component_idx>(components));
+                          }()...);
                 }
-            }()...
-        };
+                (std::make_index_sequence<x[PM_Idx].size>{},
+                 std::index_sequence<PM_Idx>{});
+              }
+            }()...};
+        }
+        (std::index_sequence_for<PM...>{});
     }
 };
